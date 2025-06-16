721999bb62fd49b72869c0823fd338c2599c9463
Kingston Bayard <149631237+K-Bayard@users.noreply.github.com>
2025-06-16 14:42:21 -0400

    Add nixos configuration

---
+++ nixos/configuration.nix
@@ -0,0 +1,48 @@
+{ config, pkgs, ... }:
+
+{
+	imports = [
+		./hardware-configuration.nix
+	];
+
+	boot.loader.systemd-boot.enable = true;
+	boot.loader.efi.canTouchEfiVariables = true;
+
+	networking.hostName = "nixos";
+	networking.networkmanager.enable = true;
+
+	time.timeZone = "America/New_York";
+
+	i18n.defaultLocale = "en_US.UTF-8";
+
+	i18n.extraLocaleSettings = {
+		LC_ADDRESS = "en_US.UTF-8";
+		LC_IDENTIFICATION = "en_US.UTF-8";
+		LC_MEASUREMENT = "en_US.UTF-8";
+		LC_MONETARY = "en_US.UTF-8";
+		LC_NAME = "en_US.UTF-8";
+		LC_NUMERIC = "en_US.UTF-8";
+		LC_PAPER = "en_US.UTF-8";
+		LC_TELEPHONE  = "en_US.UTF-8";
+		LC_TIME = "en_US.UTF-8";
+	};
+
+	users.users.poppy = {
+		isNormalUser = true;
+		description = "poppy";
+		extraGroups = [ "networkmanager" "wheel" ];
+		packages = with pkgs; [];
+	};
+
+	nixpkgs.config.allowUnfree = true;
+
+	environment.systemPackages = with pkgs; [
+
+	];
+
+	programs.hyprland.enable = true;
+
+	services.openssh.enable = true;
+
+	system.stateVersion = "25.05";
+}
