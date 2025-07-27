92a1939ae8098e93723ea16d7a21f60d3631be85
Kingston Bayard <149631237+K-Bayard@users.noreply.github.com>
2025-07-27 15:43:57 -0400

    Added my working nixos configuration

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,67 +1,117 @@
-{ config, pkgs, ... }:
+{ config, pkgs,  ... }:
 
-let
-
-in
 {
-	imports = [
-		./hardware-configuration.nix
-	];
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
 
-	nix.settings.experimental-features = [ "nix-command" "flakes" ];
-	nix.settings.download-buffer-size = 52428800;
+  nix = {
+    settings = {
+      download-buffer-size = 52428800;
+      experimental-features = [ "nix-command" ];
+    };
+  };
 
-	# Boot Loader
-	boot.loader.systemd-boot.enable = true;
-	boot.loader.efi.canTouchEfiVariables = true;
+  boot = {
+    loader = {
+      efi.canTouchEfiVariables = true;
+      grub = {
+        device = "nodev";
+      	efiSupport = true;
+	enable = true;
+	timeoutStyle = "menu";
+      };
+      timeout = 120;
+    };
+  };
+ 
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
-	# Lates Kernel
-	boot.kernelPackages = pkgs.linuxPackages_latest;
+  hardware = {
+    graphics.enable = true;
+    
+    nvidia = {
+      open = true;
+      modesetting.enable = true;
+      powerManagement.enable = true;
+    };
+  };
 
-	# Networking
-	networking.hostName = "nixos";
-	networking.networkmanager.enable = true;
+  networking = {
+    hostName = "nixos";
+    networkmanager.enable = true;
+  };
 
-	# Time Zone
-	time.timeZone = "America/New_York";
+  time.timeZone = "America/New_York";
 
-	# Locales
-	i18n.defaultLocale = "en_US.UTF-8";
-	i18n.extraLocaleSettings = {
-		LC_ADDRESS = "en_US.UTF-8";
-		LC_IDENTIFICATION = "en_US.UTF-8";
-		LC_MEASUREMENT = "en_US.UTF-8";
-		LC_MONETARY = "en_US.UTF-8";
-		LC_NAME = "en_US.UTF-8";
-		LC_NUMERIC = "en_US.UTF-8";
-		LC_PAPER = "en_US.UTF-8";
-		LC_TELEPHONE  = "en_US.UTF-8";
-		LC_TIME = "en_US.UTF-8";
-	};
+  i18n.defaultLocale = "en_US.UTF-8";
 
-	# Users
-	users.users.poppy = {
-		isNormalUser = true;
-		extraGroups = [ "networkmanager" "wheel" ];
-	};
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
 
-	# Allow non-free packages
-	nixpkgs.config.allowUnfree = true;
+  services = {
+	  xserver = {
+		  xkb = {
+		    layout = "us";
+		    variant = "";
+		  };
+		  videoDrivers = ["nvidia"];
+	  };
+	  openssh = {
+		enable = true;
+		settings = {
+			PasswordAuthentication = false;
+		};
+	  };
 
-	# System Packages
-	environment.systemPackages = with pkgs; [
-		neovim
-		git
-		ghostty
-		zsh
-		zsh-completions
-		zsh-autosuggestions
-		zsh-autocomplete
-	];
+  };
 
-	# Services to be enabled
-	services.openssh.enable = true;
+  users.users.poppy = {
+    isNormalUser = true;
+    description = "Poppy";
+    extraGroups = [ "networkmanager" "wheel" ];
+  };
 
-	# Don't Change
-	system.stateVersion = "25.05";
+  nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages = with pkgs; [
+    neovim
+    ghostty
+    git
+    zoxide
+    fastfetch
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.05"; # Did you read the comment?
+
 }
