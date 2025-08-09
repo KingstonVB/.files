a00cead5c93b6f8b15a1f10f6d504340fb34e3cd
Kingston Bayard <149631237+K-Bayard@users.noreply.github.com>
2025-08-09 17:40:02 -0400

    back to curl

--- scripts/archinstall/install.sh
+++ scripts/archinstall/install.sh
@@ -2,7 +2,7 @@
 set -e
 
 SCRIPTS=~/install-scripts/
-REPO=https://raw.githubusercontent.com/K-Bayard/.files/refs/heads/main/scripts/archinstall/
+REPO=https://raw.githubusercontent.com/K-Bayard/.files/refs/heads/main/scripts/archinstall/install-scripts
 
 install_text() {
     clear
@@ -11,7 +11,7 @@ install_text() {
 }
 
 install_text "Installing needed files"
-wget -r $REPO/install-scripts
+curl -o $REPO/aur.sh
 
 install_text "Configuring Paru and installing packages"
 source $SCRIPTS/aur.sh
