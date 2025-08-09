c0aec44fb17a69543c1966fa587103f051591541
Kingston Bayard <149631237+K-Bayard@users.noreply.github.com>
2025-08-09 17:18:08 -0400

    Silly little paru stuff

--- scripts/archinstall/install-scripts/aur.sh
+++ scripts/archinstall/install-scripts/aur.sh
@@ -3,13 +3,11 @@
 ## Install Paru ##
 ##################
 
-if ! command -v paru &>/dev/null; then
-    sudo pacman -Syu --noconfirm
-    cd /tmp
-    git clone https://aur.archlinux.org/paru.git
-    cd paru
-    makepkg -si --noconfirm
-    cd -
-    rm -rf paru
-    cd ~
-fi
+sudo pacman -Syu --noconfirm
+cd /tmp
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si --noconfirm
+cd -
+rm -rf paru
+cd ~
