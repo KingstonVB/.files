9c96faebc7abb00bb9c714f323011585ddabe5d6
Kingston Bayard <149631237+KingstonVB@users.noreply.github.com>
2025-08-29 19:06:38 -0400

    Remove step to clone repo

--- scripts/dotfiles.sh
+++ scripts/dotfiles.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 set -e
 
-git clone https://github.com/KingstonVB/.files.git ~/Documents/
-cd ~/Documents/.files/
 rm -r ~/.config/{hypr,ghostty,zed} && stow hypr ghostty zed --target=$HOME/
 rm ~/.bashrc && stow bashrc --target=$HOME/
